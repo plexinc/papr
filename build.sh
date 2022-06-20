bb946abbdaeb1d827a2761c1eca8fa565e6ec76a
Valentin Agachi <github-com@agachi.name>
2022-06-20 09:26:55 +0200

    fixup! feat: Initial public commit

--- build.sh
+++ build.sh
@@ -8,7 +8,7 @@ tsc --project ./tsconfig-esm.json
 
 # https://stackoverflow.com/a/22084103
 # This works in Linux and MacOS
-sed --in-place=.backup --expression="s/from '.\/\(.*\)';/from '.\/\1.js';/g" ./esm/*.js
+sed -i.backup -e "s/from '.\/\(.*\)';/from '.\/\1.js';/g" ./esm/*.js
 rm ./esm/*.js.backup
 
 # CommonJS
